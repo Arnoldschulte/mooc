8d773b82ca401679b317fa0c666e49285c2878a3
Arnold Schulte <arnoldschulte@gmail.com>
2019-04-24 23:44:29 +0200

    Solution exercise 71: Smart combining

---
+++ src/week4/Exercise71.java
@@ -0,0 +1,33 @@
+package week4;
+
+import java.util.ArrayList;
+
+public class Exercise71 {
+
+  public static void main(String[] args) {
+    ArrayList<Integer> list3 = new ArrayList<Integer>();
+    ArrayList<Integer> list4 = new ArrayList<Integer>();
+
+    list3.add(2);
+    list3.add(9);
+    list3.add(1);
+
+    list4.add(9);
+    list4.add(6);
+    list4.add(1);
+    list4.add(5);
+
+    smartCombine(list3, list4);
+
+    System.out.println(list3); // prints [2, 9, 1, 6, 5]
+    System.out.println(list4); // prints [9, 6, 1, 5]
+  }
+
+  public static void smartCombine(ArrayList<Integer> first, ArrayList<Integer> second) {
+    for (int i = 0; i < second.size(); i++) {
+      if (!first.contains(second.get(i))) {
+        first.add(second.get(i));
+      }
+    }
+  }
+}
