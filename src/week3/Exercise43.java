6bd88ffedff17af32f17c0363076a5bec63f671e
Arnold Schulte <arnoldschulte@gmail.com>
2019-04-24 23:28:24 +0200

    Solution exercise 70: Combining ArrayLists

--- src/week3/Exercise43.java
+++ src/week3/Exercise43.java
@@ -1,6 +1,6 @@
 package week3;
 
-public class sumOfNumbers {
+public class Exercise43 {
   public static int sum(int number1, int number2, int number3, int number4) {
     return (number1+number2+number3+number4);
   }
